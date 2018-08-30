495d7f3f958d5894652fd61e804b14dc086a621a
Abhishek Kashyap <abhishek47kashyap@gmail.com>
2018-08-29 22:59:42 -0400

    code provided by instructor

---
+++ Week 1/portopt_cvxpy.py
@@ -0,0 +1,71 @@
+"""
+Portfolio optimization with CVXPY
+See examples at http://cvxpy.org
+Author: Shabbir Ahmed
+"""
+
+import pandas as pd
+import numpy as np
+from cvxpy import *  # use cvxpy version 0.4.11
+
+# read monthly_prices.csv
+mp = pd.read_csv("monthly_prices.csv",index_col=0)
+# mp = pd.read_csv("monthly_prices_16-18.csv",index_col=0)
+mr = pd.DataFrame()
+
+# compute monthly returns
+for s in mp.columns:
+    date = mp.index[0]
+    pr0 = mp[s][date] 
+    for t in range(1,len(mp.index)):
+        date = mp.index[t]
+        pr1 = mp[s][date]
+        ret = (pr1-pr0)/pr0
+        mr.set_value(date, s, ret)
+        # Warning: set_value is deprecated and will be removed in a future release.
+        # Please use .at[] or .iat[] accessors instead
+        pr0 = pr1
+        
+# get symbol names
+symbols = mr.columns
+
+# convert monthly return data frame to a numpy matrix
+return_data = mr.as_matrix().T
+# Warning: Method .as_matrix will be removed in a future version. Use .values instead.
+
+# compute mean return
+r = np.asarray(np.mean(return_data, axis=1))
+
+# covariance
+C = np.asmatrix(np.cov(return_data))
+
+# print out expected return and std deviation
+print "----------------------"
+for j in range(len(symbols)):
+    print '%s: Exp ret = %f, Risk = %f' % (symbols[j], r[j], C[j,j]**0.5)
+   
+
+# set up optimization model
+n = len(symbols)
+x = Variable(n)
+req_return = 0.02  # hardcoded by looking at Microsoft's return in r
+ret = r.T*x
+risk = quad_form(x, C)
+prob = Problem(Minimize(risk),
+               [sum_entries(x) == 1, ret >= req_return,
+                x >= 0])
+
+# solve problem and write solution
+try:
+    prob.solve()
+    print "----------------------"
+    print "Optimal portfolio"
+    print "----------------------"
+    for s in range(len(symbols)):
+        print 'x[%s] = %f' %(symbols[s],x.value[s,0])
+    print "----------------------"
+    print 'Exp ret = %f' %(ret.value)
+    print 'risk    = %f' %((risk.value)**0.5)
+    print "----------------------"
+except:
+    print 'Error'
